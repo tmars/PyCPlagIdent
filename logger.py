41d733787f8459f64cbf23ae86da272d206c709a
Marseille Talipov <t.mars@mail.ru>
2013-06-09 13:04:46 +0300

    initial project version

--- logger.py
+++ logger.py
@@ -2,16 +2,17 @@ from libs.singleton import Singleton
 import logging
 import os
 
-log_dir = 'data/log.log'
+log_path = 'data/log.log'
 
 @Singleton
 class MyLogger(object):
     def __init__(self):
-        if not os.path.exists(log_dir):
-            os.mkdir(log_dir)
+        if not os.path.exists(log_path):
+            open(log_path, 'w+').close()
+
 
         self.logger = logging.getLogger('PlagiarismDetector')
-        hdlr = logging.FileHandler(log_dir)
+        hdlr = logging.FileHandler(log_path)
         formatter = logging.Formatter('%(asctime)s %(message)s')
         hdlr.setFormatter(formatter)
         self.logger.addHandler(hdlr)
